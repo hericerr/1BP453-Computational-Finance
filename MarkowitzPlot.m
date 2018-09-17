c6de0297f434598f1fde6e1c3b0490ed73458213
Jan Helcl <janhelcl@gmail.com>
2018-09-17 14:32:44 +0200

    Delete MarkowitzPlot.m

--- MarkowitzPlot.m
+++
@@ -1,9 +0,0 @@
-targetRets = min(Er):0.01:max(Er);
-
-for i =1:numel(targetRets)
-    [w, sigmaP(i)] = markowitzPortfolio(Er, covMat, targetRets(i));
-end
-
-plot(sigmaP, targetRets);
-grid on;
-axis([0 0.25 0.05 0.25])
